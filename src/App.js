8054ba004f201e8225e3712ef17df4ee8e124eb4
rjrobocoder <rjrobocoder@gmail.com>
2023-06-16 08:39:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' Component={Home}/>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
